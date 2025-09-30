28982be982680ca3003509aa320a0ae8f9b69f6d
“Vladislav <boromirchik@gmail.com>
2025-09-30 12:32:18 +0300

    Add forms Login and Registration to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import Provider from './providers/providers';
-import Header from './components/UI/header';
+import Provider from '../providers/providers';
+import Header from '../components/UI/header';
 
 import { siteConfig } from '@/config/site.config';
 import { layoutConfig } from '@/config/layout.config';
